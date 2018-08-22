1ea9bf0cceb44db1024680414e102a1e19ca8c0d
OkadaGenya <g_okada@waku-2.com>
2018-08-22 14:26:56 +0900

    fix text description

--- src/store/index.js
+++ src/store/index.js
@@ -9,13 +9,13 @@ const store = {
     episodeList: [
       {
         uid: '001',
-        description: '説明',
+        description: 'ゼルダの伝説/switch、万引き少年を捕まえた話、お盆の渋滞の話をしました。',
         link: '/001',
         title: 'I\'ve never redirected in my life'
-      },
+      },　
       {
         uid: '000',
-        description: '説明',
+        description: '自己紹介、Podcastの名前、東京医科大女性受験者の得点操作、Car detailingの話をしました。',
         link: '/000',
         title: 'Hello, FA cast'
       }

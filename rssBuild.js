0af9757a222302548e4bc82aa6b6936d9870cf17
OkadaGenya <g.okada.wilf+github@gmail.com>
2018-08-23 13:50:22 +0900

    fix build script setting

--- rssBuild.js
+++ rssBuild.js
@@ -3,35 +3,36 @@ const fs = require('fs');
 
 /* lets create an rss feed */
 const feed = new Podcast({
-    title: 'title',
-    description: 'description',
-    feed_url: 'http://example.com/rss.xml',
-    site_url: 'http://example.com',
-    image_url: 'http://example.com/icon.png',
-    docs: 'http://example.com/rss/docs.html',
-    author: 'Dylan Greene',
-    managingEditor: 'Dylan Greene',
-    webMaster: 'Dylan Greene',
-    copyright: '2013 Dylan Greene',
-    language: 'en',
-    categories: ['Category 1','Category 2','Category 3'],
-    pubDate: 'May 20, 2012 04:00:00 GMT',
+    title: 'FAcast',
+    description: 'FAcast',
+    feed_url: 'https://facast.net/feed.xml',
+    site_url: 'https://facast.net',
+    image_url: 'todo',
+    docs: 'todo',
+    author: 'Hayato Aokiba/Okada Genya',
+    managingEditor: 'Hayato Aokiba/Okada Genya',
+    webMaster: 'Hayato Aokiba/Okada Genya',
+    copyright: '2018 Okada Genya',
+    language: 'ja',
+    categories: ['car', 'auto', 'frontend'],
+    pubDate: 'May 20, 2012 04:00:00 GMT', // todo
     ttl: '60',
-    itunesAuthor: 'Max Nowack',
-    itunesSubtitle: 'I am a sub title',
-    itunesSummary: 'I am a summary',
-    itunesOwner: { name: 'Max Nowack', email:'max@unsou.de' },
+    itunesAuthor: 'Hayato Aokiba/Okada Genya',
+    itunesSubtitle: 'sub title',
+    itunesSummary: 'summary',
+    itunesOwner: { name: 'Okada Genya', email:'g.okada.wilf@gmail.com' },
     itunesExplicit: false,
     itunesCategory: {
-        "text": "Entertainment",
+        "text": "todo",
         "subcats": [{
-          "text": "Television"
+          "text": "todo"
         }]
     },
-    itunesImage: 'http://link.to/image.png'
+    itunesImage: 'http://link.to/todo.png'
 });
 
 /* loop over data and add to feed */
+// todo
 feed.addItem({
     title:  'item title',
     description: 'use this for the content. It can include html.',
@@ -40,9 +41,6 @@ feed.addItem({
     categories: ['Category 1','Category 2','Category 3','Category 4'], // optional - array of item categories
     author: 'Guest Author', // optional - defaults to feed author property
     date: 'May 27, 2012', // any format that js Date can parse.
-    lat: 33.417974, //optional latitude field for GeoRSS
-    long: -111.933231, //optional longitude field for GeoRSS
-    enclosure : {url:''}, // optional enclosure
     itunesAuthor: 'Max Nowack',
     itunesExplicit: false,
     itunesSubtitle: 'I am a sub title',

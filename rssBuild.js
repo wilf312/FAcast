003ce94cc681a677c41feba9af4bf72b61a609b2
OkadaGenya <g.okada.wilf+github@gmail.com>
2018-08-23 13:43:17 +0900

    add build script

---
+++ rssBuild.js
@@ -0,0 +1,60 @@
+const Podcast = require('podcast');
+const fs = require('fs');
+
+/* lets create an rss feed */
+const feed = new Podcast({
+    title: 'title',
+    description: 'description',
+    feed_url: 'http://example.com/rss.xml',
+    site_url: 'http://example.com',
+    image_url: 'http://example.com/icon.png',
+    docs: 'http://example.com/rss/docs.html',
+    author: 'Dylan Greene',
+    managingEditor: 'Dylan Greene',
+    webMaster: 'Dylan Greene',
+    copyright: '2013 Dylan Greene',
+    language: 'en',
+    categories: ['Category 1','Category 2','Category 3'],
+    pubDate: 'May 20, 2012 04:00:00 GMT',
+    ttl: '60',
+    itunesAuthor: 'Max Nowack',
+    itunesSubtitle: 'I am a sub title',
+    itunesSummary: 'I am a summary',
+    itunesOwner: { name: 'Max Nowack', email:'max@unsou.de' },
+    itunesExplicit: false,
+    itunesCategory: {
+        "text": "Entertainment",
+        "subcats": [{
+          "text": "Television"
+        }]
+    },
+    itunesImage: 'http://link.to/image.png'
+});
+
+/* loop over data and add to feed */
+feed.addItem({
+    title:  'item title',
+    description: 'use this for the content. It can include html.',
+    url: 'http://example.com/article4?this&that', // link to the item
+    guid: '1123', // optional - defaults to url
+    categories: ['Category 1','Category 2','Category 3','Category 4'], // optional - array of item categories
+    author: 'Guest Author', // optional - defaults to feed author property
+    date: 'May 27, 2012', // any format that js Date can parse.
+    lat: 33.417974, //optional latitude field for GeoRSS
+    long: -111.933231, //optional longitude field for GeoRSS
+    enclosure : {url:''}, // optional enclosure
+    itunesAuthor: 'Max Nowack',
+    itunesExplicit: false,
+    itunesSubtitle: 'I am a sub title',
+    itunesSummary: 'I am a summary',
+    itunesDuration: 12345,
+    itunesKeywords: ['javascript','podcast']
+});
+
+// cache the xml to send to clients
+const xml = feed.buildXml();
+
+fs.writeFile('dist/feed.xml', xml, (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});

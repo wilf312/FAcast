b10dfc2e783895980e13f6539d47f45ef5dd4d99
OkadaGenya <g.okada.wilf+github@gmail.com>
2018-08-23 21:29:10 +0900

    fix rss

--- rssBuild.js
+++ rssBuild.js
@@ -1,52 +1,68 @@
 const Podcast = require('podcast');
 const fs = require('fs');
+const yaml = require('js-yaml');
+
+let config
+let episode
+try {
+  episode = yaml.safeLoad(fs.readFileSync('src/assets/episode.yml', 'utf8'));
+  config = yaml.safeLoad(fs.readFileSync('src/assets/config.yml', 'utf8'));
+} catch (e) {
+  console.log(e);
+}
+
+const {episodeList} = episode
+const {domain, author} = config
 
 /* lets create an rss feed */
 const feed = new Podcast({
-    title: 'FAcast',
-    description: 'FAcast',
-    feed_url: 'https://facast.net/feed.xml',
-    site_url: 'https://facast.net',
-    image_url: 'https://facast.net/static/ogp.jpg',
-    docs: '雑談系Podcast',
-    author: 'Hayato Aokiba/Okada Genya',
-    managingEditor: 'Hayato Aokiba/Okada Genya',
-    webMaster: 'Hayato Aokiba/Okada Genya',
-    copyright: '2018 Okada Genya',
-    language: 'ja',
-    categories: ['car', 'auto', 'frontend'],
-    pubDate: 'May 20, 2012 04:00:00 JST',
-    ttl: '60',
-    itunesAuthor: 'Hayato Aokiba/Okada Genya',
-    itunesSubtitle: '雑談系Podcast',
-    itunesSummary: '雑談系Podcast',
-    itunesOwner: { name: 'Okada Genya', email:'g.okada.wilf@gmail.com' },
-    itunesExplicit: false,
-    itunesCategory: {
-        "text": "Technology",
-        "subcats": [{
-          "text": "Technology"
-        }]
-    },
-    itunesImage: 'https://facast.net/static/ogp.jpg' // 1400? http://cdn.rebuild.fm/images/icon1400.jpg
+  title: 'FAcast',
+  description: 'FAcast',
+  feed_url: `${domain}/feed.xml`,
+  site_url: `${domain}`,
+  image_url: `${domain}/static/ogp.jpg`,
+  docs: '雑談系Podcast',
+  author: author,
+  managingEditor: author,
+  webMaster: author,
+  copyright: '2018 Okada Genya',
+  language: 'ja',
+  categories: ['car', 'auto', 'frontend'],
+  pubDate: episodeList[0].pubDate,
+  ttl: '60',
+  itunesAuthor: author,
+  itunesSubtitle: '雑談系Podcast',
+  itunesSummary: '雑談系Podcast',
+  itunesOwner: { name: 'Okada Genya', email:'g.okada.wilf@gmail.com' },
+  itunesExplicit: false,
+  itunesCategory: {
+    "text": "Technology",
+    "subcats": [{
+      "text": "Technology"
+    }]
+  },
+  itunesImage: `${domain}/static/ogp.jpg` // 1400? http://cdn.rebuild.fm/images/icon1400.j`
 });
 
 /* loop over data and add to feed */
-// todo
-feed.addItem({
-    title:  'item title',
-    description: 'use this for the content. It can include html.',
-    url: 'http://example.com/article4?this&that', // link to the item
-    guid: '1123', // optional - defaults to url
-    categories: ['Category 1','Category 2','Category 3','Category 4'], // optional - array of item categories
-    author: 'Guest Author', // optional - defaults to feed author property
-    date: 'May 27, 2012', // any format that js Date can parse.
-    itunesAuthor: 'Max Nowack',
-    itunesExplicit: false,
-    itunesSubtitle: 'I am a sub title',
-    itunesSummary: 'I am a summary',
-    itunesDuration: 12345,
-    itunesKeywords: ['javascript','podcast']
+const feedConst = {
+  itunesAuthor: author,
+  itunesExplicit: false
+}
+episodeList.forEach(({
+  title,
+  description,
+  pubDate,
+  duration,
+  uid
+}) => {
+  feed.addItem(Object.assign({}, {
+    title,
+    description,
+    url: `${domain}/${uid}`, // link to the item
+    date: pubDate, // any format that js Date can parse.
+    itunesDuration: duration
+  }, feedConst));
 });
 
 // cache the xml to send to clients

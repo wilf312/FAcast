4b6658dd28781aabc1eb9e1a851ef26afb1c4d37
OkadaGenya <g.okada.wilf+github@gmail.com>
2018-08-31 10:55:28 +0900

    add goto top & add page transition

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,12 @@
   <div id="app">
     <Header />
     <div class="contents">
-      <router-view/>
+      <transition
+        name="fade"
+        mode="out-in"
+      >
+        <router-view class="child-view"></router-view>
+      </transition>
     </div>
   </div>
 </template>
@@ -29,5 +34,17 @@ export default {
   display: flex;
   flex-direction: column;
   margin: 0 auto;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
 }
 </style>

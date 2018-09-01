ea41162272076b27dd918935afbbd6c276c93ab5
OkadaGenya <g.okada.wilf+github@gmail.com>
2018-09-01 12:51:03 +0900

    add rss config

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="Footer">
+    aaaaaaaaaaaaaaaaa
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  computed: {
+  }
+}
+</script>
+
+<style scoped>
+.Footer {
+  background: #fafafa;
+}
+
+</style>
